1c4c163c435f2a567129229d24d60df3afcf87d9
sweetybolisetty2002-creator <sweetybolisetty2002@gmail.com>
2025-11-10 17:27:54 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,27 @@
 from flask import Flask
-from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object('config.Config')
+from .models import db, User
+from .views import views
 
-# Setup session management
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your-db-file.db'
+    app.config['BLOB_ACCOUNT'] = 'yourstorageaccount'
+    app.config['BLOB_STORAGE_KEY'] = 'yourkey'
+    app.config['BLOB_CONTAINER'] = 'yourcontainer'
 
-from . import views  # Import routes from views.py
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'views.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    app.register_blueprint(views)
+    return app
