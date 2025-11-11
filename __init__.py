defbdf392fee9da7feb15fd80a372047bfbe26dd
sweetybolisetty2002-creator <sweetybolisetty2002@gmail.com>
2025-11-11 14:27:39 +0530

    __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,11 @@ from .views import views
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SECRET_KEY'] = 'GvnN4QwQN1wKOaa0RlNDS_G3DDvMvXh9tgmevZ24G5A='
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your-db-file.db'
-    app.config['BLOB_ACCOUNT'] = 'yourstorageaccount'
-    app.config['BLOB_STORAGE_KEY'] = 'yourkey'
-    app.config['BLOB_CONTAINER'] = 'yourcontainer'
+    app.config['BLOB_ACCOUNT'] = 'images11'
+    app.config['BLOB_STORAGE_KEY'] = ''
+    app.config['BLOB_CONTAINER'] = 'images'
 
     db.init_app(app)
 
