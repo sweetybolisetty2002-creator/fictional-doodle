2f2255106d26c68b4b4b190a55ad7acaf8d23682
sweetybolisetty2002-creator <sweetybolisetty2002@gmail.com>
2025-11-11 18:12:59 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,30 +1,19 @@
+"""
+The flask application package.
+"""
+import logging
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_session import Session
 
-from .models import db, User
-from .views import views
+app = Flask(__name__)
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'GvnN4QwQN1wKOaa0RlNDS_G3DDvMvXh9tgmevZ24G5A='
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your-db-file.db'
-    app.config['BLOB_ACCOUNT'] = 'images11'
-    app.config['BLOB_STORAGE_KEY'] = ''
-    app.config['BLOB_CONTAINER'] = 'images'
-
-    db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'views.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    app.register_blueprint(views)
-    return app
-
-# Instantiate app for Azure (and imports)
-app = create_app()
+import FlaskWebProject.views
